634bc5410c999400b96a1beb46af7f4961b23ff2
maxime.quentin <maxime.quentin@datadoghq.com>
2020-02-07 10:26:21 +0100

    add rollup serve option for dev

--- dd-vuejs-example/rollup.config.js
+++ dd-vuejs-example/rollup.config.js
@@ -1,8 +1,11 @@
 import vue from 'rollup-plugin-vue'
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import serve from 'rollup-plugin-serve'
 
-export default {
+const isWatch = process.env.NODE_ENV === `watch`
+
+var config = {
   input: 'src/index.js',
   output: {
     format: 'esm',
@@ -10,9 +13,24 @@ export default {
   },
   plugins: [
     resolve(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.BUILD)
-    }),
     vue()
   ]
 }
+
+if (isWatch) {
+  config.plugins.push(serve({
+    open: true,
+    host: 'localhost',
+    port: 10001,
+    openPage: '/index.js',
+    contentBase: ['dist']
+  }))
+}
+else{
+  config.plugins.push(
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.BUILD)
+    }))
+}
+
+export default config 
